4ef60274658f6a8d1cdbd835267c2e70eff2e551
entrepriseprosellez62 <entreprisesellezpro62@gmail.com>
2026-09-15 12:48:33 +0200

    Delete Script.js

--- Script.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.menu').addEventListener('click',()=>document.querySelector('.nav nav').classList.toggle('open'));
-document.querySelectorAll('.nav nav a').forEach(a=>a.addEventListener('click',()=>document.querySelector('.nav nav').classList.remove('open')));
-document.getElementById('year').textContent=new Date().getFullYear();
